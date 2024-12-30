414a8f8495bebc60edb232824dfbf5408df15afb
adityapatil687 <adityapatil687@gmail.com>
2024-12-30 23:28:41 +0530

    Native-Wind Setup Done

---
+++ app/Home.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View className='py-2 px-5'>
+      <Text className='text-3xl'>Aditya Patil</Text>
+      <Text className=''>Manan Foods</Text>
+    </View>
+  )
+}
+
+export default Home
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import Home from "./Home";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
+  );
+}
